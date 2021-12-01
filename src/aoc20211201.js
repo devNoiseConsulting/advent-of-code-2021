fc1fe0a7d3f6e29461c3a398b690f82507b9ba35
Michael Flynn <flynnmj@devnoise.com>
2021-12-01 11:17:33 -0500

    Day 1

---
+++ src/aoc20211201.js
@@ -0,0 +1,58 @@
+var fs = require("fs");
+var path = require("path");
+
+let file = path.join(__dirname, "..", "data", "day1.txt");
+var text = fs.readFileSync(file, "utf-8").trim();
+var numbers = text.split("\n");
+numbers = numbers.map((i) => parseInt(i));
+
+let depthCounter = function (accumulator, currentValue, i, array) {
+  if (i > 0) {
+    if (currentValue > array[i - 1]) {
+      accumulator++;
+    }
+  }
+
+  return accumulator;
+};
+
+let sum = numbers.reduce(depthCounter, 0);
+
+console.log(sum);
+
+console.log("---");
+
+sum = numbers
+  .map((currentValue, i, array) => {
+    return array[i] + array[i + 1] + array[i + 2];
+  })
+  .filter((currentValue) => Number.isInteger(currentValue))
+  .reduce(depthCounter, 0);
+
+console.log(sum);
+
+console.log("---");
+
+sum = numbers
+  .map((currentValue, i, array) => {
+    return array[i] - array[i - 1];
+  })
+  .filter((currentValue) => Number.isInteger(currentValue))
+  .filter((currentValue) => currentValue > 0).length;
+
+console.log(sum);
+
+console.log("---");
+
+sum = numbers
+  .map((currentValue, i, array) => {
+    return array[i] + array[i + 1] + array[i + 2];
+  })
+  .filter((currentValue) => Number.isInteger(currentValue))
+  .map((currentValue, i, array) => {
+    return array[i] - array[i - 1];
+  })
+  .filter((currentValue) => Number.isInteger(currentValue))
+  .filter((currentValue) => currentValue > 0).length;
+
+console.log(sum);
